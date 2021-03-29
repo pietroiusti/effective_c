bc0b76cc688b1878232be7fa9088a2abb9a405c6
glptrst <giulio.pietroiusti@gmail.com>
2021-03-30 01:46:54 +0200

    fix.

--- 5/exercises/3.c
+++ 5/exercises/3.c
@@ -8,18 +8,25 @@
 int absolute_value(int a);
 
 int main(void) {
-    printf("%i\n", absolute_value(-2147483648));
+    printf("%i\n", absolute_value(-2147483647));
+    printf("%i\n", absolute_value(-2147483648)); // too big for a positive
     return 0;
 }
 
 int absolute_value(int a) {
-    if (a < 0) {
-	if (a >= INT_MIN)
+    if (a < 0)
+	if (a == INT_MIN)
 	    return -1;
-	else
-	    return -a;
-    } else if (a > INT_MAX) {
-	return -1;
-    }
+	else return a;
     return a;
 }
+
+/* 
+Alternative: (thanks D.)
+
+int absolute_value(int a) {
+    if (a < 0) a = -a;
+    if (a < 0) return -1;
+    return a;
+}
+*/
